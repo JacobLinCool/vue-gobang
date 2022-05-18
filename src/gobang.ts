da9bcf929d7bfa011b2ac1774f6ba7854e6b082f
JacobLinCool <jacoblincool@gmail.com>
2022-05-18 16:08:08 +0800

    feat: move UI state control to Board

--- src/gobang.ts
+++ src/gobang.ts
@@ -6,3 +6,81 @@ export enum BoardState {
 
 export const cols = 15;
 export const rows = 15;
+
+/**
+ * Check if there is a winner.
+ * @param board The board to check.
+ * @returns The winner, or BoardState.Empty if there is no winner. And the stones.
+ */
+export function check_winner(board: BoardState[]) {
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === BoardState.Empty) {
+            continue;
+        }
+
+        const state = board[i];
+        const [x, y] = [i % cols, Math.floor(i / cols)];
+
+        // check -
+        if (x < cols - 4) {
+            const stones = [i];
+            for (let j = 1; j < 5; j++) {
+                if (board[i + j] === state) {
+                    stones.push(i + j);
+                } else {
+                    break;
+                }
+            }
+            if (stones.length === 5) {
+                return { winner: state, stones };
+            }
+        }
+
+        // check |
+        if (y < rows - 4) {
+            const stones = [i];
+            for (let j = 1; j < 5; j++) {
+                if (board[(y + j) * cols + x] === state) {
+                    stones.push((y + j) * cols + x);
+                } else {
+                    break;
+                }
+            }
+            if (stones.length === 5) {
+                return { winner: state, stones };
+            }
+        }
+
+        // check \
+        if (x < cols - 4 && y < rows - 4) {
+            const stones = [i];
+            for (let j = 1; j < 5; j++) {
+                if (board[(y + j) * cols + x + j] === state) {
+                    stones.push((y + j) * cols + x + j);
+                } else {
+                    break;
+                }
+            }
+            if (stones.length === 5) {
+                return { winner: state, stones };
+            }
+        }
+
+        // check /
+        if (x > 3 && y < rows - 4) {
+            const stones = [i];
+            for (let j = 1; j < 5; j++) {
+                if (board[(y + j) * cols + x - j] === state) {
+                    stones.push((y + j) * cols + x - j);
+                } else {
+                    break;
+                }
+            }
+            if (stones.length === 5) {
+                return { winner: state, stones };
+            }
+        }
+    }
+
+    return { winner: BoardState.Empty, stones: [] };
+}
